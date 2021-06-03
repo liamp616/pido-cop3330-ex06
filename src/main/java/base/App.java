0273399a6d4d315660e6b72619e59331d02e37bc
Liam <xlime1p@gmail.com>
2021-06-02 23:46:23 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,61 @@
+package base;
+
+import java.util.Scanner;
+
+/*
+Exercise 6 - Retirement Calculator
+
+Your computer knows what the current year is, which means you can incorporate that into your programs.
+You just have to figure out how your programming language can provide you with that information.
+
+Create a program that determines how many years you have left until retirement and the year you can retire.
+It should prompt for your current age and the age you want to retire and display the output as shown in the example that follows.
+
+Example Output
+
+What is your current age? 25
+At what age would you like to retire? 65
+You have 40 years left until you can retire.
+It's 2015, so you can retire in 2055.
+
+Constraints
+
+Again, be sure to convert the input to numerical data before doing any math.
+Don’t hard-code the current year into your program. Get it from the system time via your programming language.
+
+Challenge
+Handle situations where the program returns a negative number by stating that the user can already retire.
+ */
+
+public class App {
+    Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        App myApp = new App();
+
+        String ageInput = myApp.readAgeInput();
+        int age = Integer.parseInt(ageInput);
+
+        String likeToRetire = myApp.readLTR();
+        int LTR = Integer.parseInt(likeToRetire);
+
+        myApp.retire(age, LTR);
+    }
+
+    public void retire(int age, int LTR) {
+        System.out.println("You have " + (LTR - age) + " years left until you can retire.");
+        System.out.println("It's 2015, so you can retire in " + (2015+(LTR - age)));
+    }
+
+    public String readLTR() {
+        System.out.print("At what age would you like to retire? ");
+        String LTR = in.nextLine();
+        return LTR;
+    }
+
+    public String readAgeInput() {
+        System.out.print("What is your current age? ");
+        String ageInput = in.nextLine();
+        return ageInput;
+    }
+}
